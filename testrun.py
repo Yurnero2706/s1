1e7c214b7814b47cbc1c4b0403dfcec8de24d2ed
Yurnero2706 <ncnguyencva@gmail.com>
2026-04-24 14:33:13 +0900

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -1,22 +1,34 @@
-from vllm import LLM, SamplingParams
-from transformers import AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-model = LLM(
-    "simplescaling/s1.1-32B",
-    tensor_parallel_size=2,
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+model_name = "simplescaling/s1.1-32B"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
 )
-tok = AutoTokenizer.from_pretrained("simplescaling/s1-32B")
-
-stop_token_ids = tok("<|im_end|>")["input_ids"]
-
-sampling_params = SamplingParams(
-    max_tokens=32768,
-    min_tokens=0,
-    stop_token_ids=stop_token_ids,
-)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 prompt = "How many r in raspberry"
-prompt = "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n" + prompt + "<|im_end|>\n<|im_start|>assistant\n"
+messages = [
+    {"role": "system", "content": "You are a helpful and harmless assistant. You are Qwen developed by Alibaba. You should think step-by-step."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-o = model.generate(prompt, sampling_params=sampling_params)
-print(o[0].outputs[0].text)
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
